72c09936799d35cf7e3f1d8a721a6619d59fb12b
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:41:15 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -184,3 +184,4 @@ detect_os() {
                 PKG_MANAGER="emerge"
                 PKG_UPDATE="emerge --sync -q"
                 PKG_INSTALL="emerge -av"
+                PYTHON_PKG="dev-python/pip"
