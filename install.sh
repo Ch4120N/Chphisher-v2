9ef54d8c6df12617ccbcd56700fd7a05f82521e7
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:42:30 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -289,3 +289,4 @@ detect_os() {
 install_packages() {
     if [[ -n "${PIP_PKG:-}" ]]; then
         run_with_spinner "Updating package lists" sh -c "$PKG_UPDATE"
+        run_with_spinner "Installing Python, PHP, and pip" sh -c "$PKG_INSTALL $PYTHON_PKG $PHP_PKG $PIP_PKG"
