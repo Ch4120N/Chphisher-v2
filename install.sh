08a9d4563c0186a143cbbb69e3b0fb01755362fb
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:40:44 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -151,3 +151,4 @@ detect_os() {
                 PKG_UPDATE="pacman -Sy --noconfirm"
                 PKG_INSTALL="pacman -S --noconfirm"
                 PYTHON_PKG="python python-pip"
+                PHP_PKG="php"
