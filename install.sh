d81b634821989bf83900117710a2c9e8086502eb
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:41:45 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -219,3 +219,4 @@ detect_os() {
                     PKG_INSTALL="zypper install -y"
                     PYTHON_PKG="python3 python3-pip"
                     PHP_PKG="php"
+                elif command -v apk &>/dev/null; then
