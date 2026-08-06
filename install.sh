1fb91cf6f1f4b5046baff2c615ca151c64cbdef1
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:42:35 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -300,3 +300,4 @@ install_packages() {
 install_python_deps() {
     if [[ ! -f requirements.txt ]]; then
         print_warn "requirements.txt not found in current directory. Skipping pip install."
+        return
