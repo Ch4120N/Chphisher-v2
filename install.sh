d673cd595bb81f316892eec92cf595cb3fe8941a
Ch4120N <Ch4120N@Proton.me>
2026-08-06 04:42:53 +0430

    Updated install.sh script dependencies

--- install.sh
+++ install.sh
@@ -338,3 +338,4 @@ main() {
     # Check root (except Termux)
     if [[ "$OS_NAME" != "termux" ]] && [[ "$(id -u)" -ne 0 ]]; then
         print_error "This script must be run as root (or with sudo) to install system packages."
+        exit 1
